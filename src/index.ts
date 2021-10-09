d80a7985d17892f4bf247ab0f3f3f0ff9f33e2fd
Markaronin <jmarkroylance@gmail.com>
2021-10-09 11:46:39 -0600

    Add getCookie function

--- src/index.ts
+++ src/index.ts
@@ -48,3 +48,19 @@ export function groupBy<Item, Key extends string | number>(
 export function capitalize(s: string): string {
   return s.charAt(0).toUpperCase() + s.slice(1);
 }
+
+export function getCookie(cname: string): string | undefined {
+  const name = cname + "=";
+  const decodedCookie = decodeURIComponent(document.cookie);
+  const ca = decodedCookie.split(";");
+  for (let i = 0; i < ca.length; i++) {
+    let c = ca[i];
+    while (c.charAt(0) == " ") {
+      c = c.substring(1);
+    }
+    if (c.indexOf(name) == 0) {
+      return c.substring(name.length, c.length);
+    }
+  }
+  return undefined;
+}
